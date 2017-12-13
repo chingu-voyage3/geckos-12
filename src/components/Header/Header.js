5c64bf317944bddb86dbe87c44fd5e37598d910b
Petru Conduraru <petruconduraru@outlook.com>
2017-12-13 10:24:53 +0200

    Added a h1 test element

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,12 @@
+// The Header componen
+
 import React from 'react';
 
 const Header = () => {
   render() {
     return(
-
-    );
+      <h1>Wellcome to Story Of Banana App</h1>
   }
-}
+};
 
 export default header;
