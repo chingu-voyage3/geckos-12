3b0f912ad874bcb2c601361229f7e101c45bf889
Petru Conduraru <petruconduraru@outlook.com>
2017-12-13 11:23:21 +0200

    Fixed sample header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,9 @@
-// The Header componen
+// The Header component
 
 import React from 'react';
 
-const Header = () => {
-  render() {
-    return(
+const header = () => (
       <h1>Wellcome to Story Of Banana App</h1>
-  }
-};
+);
 
 export default header;
