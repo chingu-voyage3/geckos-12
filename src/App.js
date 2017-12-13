52dbcb914297720f7eb36a6c4ca87f5a23843a79
Petru Conduraru <petruconduraru@outlook.com>
2017-12-13 11:09:32 +0200

    Deleted app.css, added Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
-import './App.css';
+
+import Header from './components/Header/Header';
 
 class App extends Component {
   render() {
     return (
       <div>
+        <Header />    
       </div>
     );
   }
