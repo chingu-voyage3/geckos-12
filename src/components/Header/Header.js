d0ef78b4629d5bd185beb6676058f81b645d291c
Petru Conduraru <petruconduraru@outlook.com>
2017-12-21 08:55:44 +0200

    Header inner splitt earnings and nav

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,33 +2,41 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <nav>
-      <div className="navWide">
+    <header>
+      <div className="headerDesktop">
         <div className="wideDiv">
-        <a href="#">Time counter</a>
-        <a href="#">Miguel</a>
-        <a href="#">Lidl</a>
-        <a href="#">Schwarz</a>
-        <a href="#">English</a>
-        <a href="#">Facebook</a>
-        <a href="#">Twitter</a>
-        <a href="#">Get Engaged</a>
-        </div>
-      </div>
-      <div className="navNarrow">
-        <i className="fa fa-bars fa-2x"></i>
-          <div className="narrowDiv">
-            <a href="#">Time counter</a>
-            <a href="#">Miguel</a>
-            <a href="#">Lidl</a>
-            <a href="#">Schwarz</a>
-            <a href="#">English</a>
+          <ul>
+            <li>Time counter</li>
+            <li>Miguel</li>
+            <li>Lidl</li>
+            <li>Schwarz</li>
+          </ul>
+          <nav>
+            <a href="#">Languages</a>
             <a href="#">Facebook</a>
             <a href="#">Twitter</a>
             <a href="#">Get Engaged</a>
+          </nav>
+        </div>
+      </div>
+      <div className="headerMobile">
+        <i className="fa fa-bars fa-2x"></i>
+          <div className="narrowDiv">
+            <ul>
+              <li>Time counter</li>
+              <li>Miguel</li>
+              <li>Lidl</li>
+              <li>Schwarz</li>
+            </ul>
+            <nav>
+              <a href="#">Languages</a>
+              <a href="#">Facebook</a>
+              <a href="#">Twitter</a>
+              <a href="#">Get Engaged</a>
+            </nav>
           </div>
       </div>
-    </nav>      
+    </header>      
   );    
 };
 
