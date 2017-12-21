05f1d3f9872aff0d6312b3a3e77cdd18df6dee01
Petru Conduraru <petruconduraru@outlook.com>
2017-12-21 08:30:13 +0200

    Created nav for desktop and mobile

--- src/components/Header/Earnings/Earning/Earning.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const earning = () => {
-  return (
-    <h2>Earning component</h2>
-  );
-};
-
-export default earning;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,35 @@
 import React from 'react';
 
-import Earnings from './Earnings/Earnings.js';
-import Navigation from './Navigation/Navigation';
-
-const header = () => {
+const Header = () => {
   return (
-    <React.Fragment>
-      <Earnings />
-      <Navigation />
-    </React.Fragment>      
+    <nav>
+      <div className="navWide">
+        <div className="wideDiv">
+        <a href="#">Time counter</a>
+        <a href="#">Miguel</a>
+        <a href="#">Lidl</a>
+        <a href="#">Schwarz</a>
+        <a href="#">English</a>
+        <a href="#">Facebook</a>
+        <a href="#">Twitter</a>
+        <a href="#">Get Engaged</a>
+        </div>
+      </div>
+      <div className="navNarrow">
+        <i className="fa fa-bars fa-2x"></i>
+          <div className="narrowDiv">
+            <a href="#">Time counter</a>
+            <a href="#">Miguel</a>
+            <a href="#">Lidl</a>
+            <a href="#">Schwarz</a>
+            <a href="#">English</a>
+            <a href="#">Facebook</a>
+            <a href="#">Twitter</a>
+            <a href="#">Get Engaged</a>
+          </div>
+      </div>
+    </nav>      
   );    
 };
 
-export default header;
+export default Header;
